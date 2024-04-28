d820657fc00260d064bb5ee152f3efda23ee5bca
Bruno Gonçalves <brunogon463@gmail.com>
2024-04-28 17:09:09 -0300

    updates

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-var topicos = [ "Análise BI", 
-                "Análise de Dados", 
-                "Gestão de projetos de TI",
-                "Análise de UX",
-                "Programação",
-                "Qualidade de software"
-            ]; 
-var indiceAtual = 0; 
+var topicos = [
+    { nome: "Segurança da Informação", link: "seguranca.html" },
+    { nome: "Análise BI", link: "analisebi.html" },
+    { nome: "Análise de Dados", link: "ads.html" },
+    { nome: "Gestão de projetos de TI", link: "gestao.html" },
+    { nome: "Análise de UX", link: "analiseux.html" },
+    { nome: "Programação", link: "programacao.html" },
+    { nome: "Qualidade de software", link: "qds.html" },
+];
+
+var indiceAtual = 0;
+
 function mudarTopico() {
-    indiceAtual++; // Avança para o próximo tópico
+    indiceAtual++;
     if (indiceAtual >= topicos.length) {
-        indiceAtual = 0; 
+        indiceAtual = 0; // Voltar ao primeiro tópico
     }
-    document.querySelector(".topics").innerText = topicos[indiceAtual];
+    document.getElementById("topicLink").href = topicos[indiceAtual].link;
+    document.getElementById("topicLink").textContent = topicos[indiceAtual].nome;
 }
